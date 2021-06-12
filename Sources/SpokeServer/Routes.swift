c365970ce10b804c12a35810a5910090339a84a5
Joannis orlandos <joannis@orlandos.nl>
2021-06-12 20:34:22 +0200

    Roll back encoding

--- Sources/SpokeServer/Routes.swift
+++ Sources/SpokeServer/Routes.swift
@@ -228,7 +228,7 @@ func registerRoutes(to routes: RoutesBuilder) {
             from: currentUserDevice,
             to: recipientDevice
         )
-        let encoded = try BSONEncoder().encode(message)
+        let encoded = try BSONEncoder().encode(chatMessage)
         
         return req.application.webSocketManager.websocket(forDevice: recipientDevice).flatMap { webSocket -> EventLoopFuture<Void> in
             if let webSocket = webSocket {
