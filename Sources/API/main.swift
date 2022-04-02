fce03ab298da1b8e6e865d92a0f09a444a3c6c63
Joannis Orlandos <joannis@orlandos.nl>
2022-04-02 09:39:45 +0200

    Apply maxByteCount globally

--- Sources/API/main.swift
+++ Sources/API/main.swift
@@ -8,6 +8,7 @@ try app.initializeMongoDB(connectionString: "mongodb://localhost/workspaces")
 
 let p8 = "LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tCk1JR1RBZ0VBTUJNR0J5cUdTTTQ5QWdFR0NDcUdTTTQ5QXdFSEJIa3dkd0lCQVFRZ090aW9KWTdZS25CaXNndGsKeW9XTGwzeStZZ2RLZjJ4TGRPS2lRaWoraitlZ0NnWUlLb1pJemowREFRZWhSQU5DQUFUTmEvd0k5MkFSY0lqUwpjRkt3Mkg3Y0txOFFtWEh0cGlkK2lxMzU2QnJNaVh4cmJIcFplT2pnU1ZUUXYxS0FZZ1YwOGhhMnVSa0srNk51CmttY0loSHBJCi0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS0="
 
+app.routes.defaultMaxBodySize = 4_500_000
 app.apns.configuration = try .init(
     authenticationMethod: .jwt(
         key: .private(pem: Data(base64Encoded: p8)!),
